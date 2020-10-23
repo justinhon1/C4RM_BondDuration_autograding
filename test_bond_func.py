eac07c252302b67ca7427dbecd19a66f56e64d48
justinhon1 <70420643+justinhon1@users.noreply.github.com>
2020-10-23 17:53:25 -0400

    Update test_bond_func.py

--- test_bond_func.py
+++ test_bond_func.py
@@ -1,4 +1,6 @@
 import bond_func
 
 def test_Bond():
-    assert round(bond_func.BondPricer(0.03,2000000,0.04,10,2)) == 2171686
+    assert round(bond_func.getBondDuration(0.03,0.04,10,1)) == 0
+    
+
